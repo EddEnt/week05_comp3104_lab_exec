bd34145e20501c81805014eb21895638f67a03c3
EddEnt <61289536+EddEnt@users.noreply.github.com>
2020-10-05 20:31:15 -0400

    Completed the instructions
    Followed along with the word document instructions to complete the lab.

---
+++ build/precache-manifest.c8b23655836c25e7a71750ed49aed9b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "835e793d61aedae04c61",
+    "url": "/static/css/main.ce962607.chunk.css"
+  },
+  {
+    "revision": "835e793d61aedae04c61",
+    "url": "/static/js/main.835e793d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "a4ef7d9f1bac9a40455d5e5f03de05ff",
+    "url": "/index.html"
+  }
+];
